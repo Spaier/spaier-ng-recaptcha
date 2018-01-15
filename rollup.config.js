8a060a5af245d0035124c623a27eb1ea55e513a2
Pavel Levchuk <AnimeshnigX@gmail.com>
2018-01-15 14:18:42 +0300

    build(package): update build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+
 const globals = {
   '@angular/core': 'ng.core',
   '@angular/common': 'ng.common',
@@ -8,19 +10,22 @@ const globals = {
   'rxjs/Observable': 'Rx',
   'rxjs/Observer': 'Rx',
   'rxjs/BehaviorSubject': 'Rx',
-  'rxjs/Subscription' : 'Rx'
+  'rxjs/Subscription': 'Rx'
 };
 export default {
   input: './dist/modules/spaier-ng-recaptcha.es5.js',
   output: {
     file: './dist/bundles/spaier-ng-recaptcha.umd.js',
-    format: 'umd'
+    format: 'umd',
+    sourcemap: true,
+    amd: { id: 'spaier-ng-recaptcha' },
   },
   exports: 'named',
   moduleName: 'ng.spaierRecaptcha',
   plugins: [
     resolve(),
-    commonjs()
+    commonjs(),
+    sourcemaps()
   ],
   external: Object.keys(globals),
   globals: globals,
--- scripts/map-sources.js
+++
@@ -1,7 +0,0 @@
-const sorcery = require('sorcery');
-var argv = require('yargs')
-  .alias('f', 'file')
-  .argv;
-sorcery.load(argv.file).then(function(chain) {
-  chain.write();
-});
