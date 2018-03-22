d20016d68349de5ba4e94d3c4240967fc4c7665a
Pavel Levchuk <SpaierInstinct@gmail.com>
2018-03-22 16:14:35 +0300

    feat(loader): set language of recaptcha with RecaptchaLoaderModue.withLanguage

--- src/loader/recaptcha-loader.module.ts
+++ src/loader/recaptcha-loader.module.ts
@@ -1,6 +1,7 @@
-import { Optional, NgModule, SkipSelf } from '@angular/core';
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core'
 
-import { RecaptchaLoaderService } from './recaptcha-loader.service';
+import { RecaptchaLoaderService } from './recaptcha-loader.service'
+import { RECAPTCHA_LANGUAGE } from './recaptcha-language.token'
 
 /**
  * Module for loading recaptcha.
@@ -13,10 +14,17 @@ export class RecaptchaLoaderModule {
 	 * Prevents module from being imported twice.
 	 * @param parentModule Another instance of this module.
 	 */
-	constructor( @Optional() @SkipSelf() parentModule: RecaptchaLoaderModule) {
+	constructor(@Optional() @SkipSelf() parentModule: RecaptchaLoaderModule) {
 		if (parentModule) {
 			throw new Error(
-				'RecaptchaLoaderModule is already loaded. Import it only in the CoreModule or AppModule.');
+				'RecaptchaLoaderModule is already loaded. Import it only in the CoreModule or AppModule.')
+		}
+	}
+	public static withLanguage(language: string): ModuleWithProviders {
+		return {
+			ngModule: RecaptchaLoaderModule, providers: [
+				{ provide: RECAPTCHA_LANGUAGE, useValue: language }
+			]
 		}
 	}
 }
